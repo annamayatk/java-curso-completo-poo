f2ef63090959ace1b0af354e2de852fe4e132f62
Anna May <annamayatk@gmail.com>
2026-06-29 23:00:26 -0300

    exercicios iniciais Nivelamento em java

---
+++ primeiro_projeto/src/Exercicio01.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Exercicio01 {
+
+	public static void main(String[] args) {
+
+		int x, y;
+
+		Scanner sc = new Scanner(System.in);
+		x = sc.nextInt();
+		y = sc.nextInt();
+
+		System.out.println("SOMA = " + Math.addExact(x, y));
+
+		sc.close();
+
+	}
+}
---
+++ primeiro_projeto/src/Program.java
@@ -0,0 +1,13 @@
+
+public class Program {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Olá mundo!");
+		System.out.println("Bom dia!");
+		
+		int y = 32;
+		
+		System.out.println(y);
+	}
+}
